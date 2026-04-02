3b6ec1bb427088bb061ce4737537167a1b0052be
mikkiihyun <mikkii@snu.ac.kr>
2026-04-02 09:39:26 +0900

    sh: Implement division

--- calc.py
+++ calc.py
@@ -1,8 +1,20 @@
-def add(a, b):
-    return a + b
+def main():
+    print("Let's implement division. Type two numbers for x and y.")
 
-def div(a, b):
-    if b == 0:
-        print("div 0!")
+    x = int(input("x > "))
+    y = int(input("y > "))
+
+    result = divide(x, y)
+    if result is not None:
+        print(f"{x} / {y} = {result}")
+def add(x, y):
+    return x + y
+
+def divide(x, y):
+    if y == 0:
+        print("Error: cannot divide by zero.")
         return None
-    return a / b
+    return x / y
+
+if __name__ == "__main__":
+    main()
